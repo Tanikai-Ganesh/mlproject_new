8ab713dddf67041680aacca17ee503ee551a8b19
Tanikai-Ganesh <tanikaiceg02@gmail.com>
2025-02-27 23:37:40 +0530

    Final

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -53,7 +53,7 @@ class ModelTrainer:
 
             params={
                 "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                   'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                     # 'splitter':['best','random'],
                     # 'max_features':['sqrt','log2'],
                 },
